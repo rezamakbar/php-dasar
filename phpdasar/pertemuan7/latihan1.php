4607ff425dd8ca92ff8597bb2175edd6dd3669f3
rezamakbar <rezamakbar13@gmail.com>
2020-07-15 11:43:32 +0700

    tambah

--- phpdasar/pertemuan7/latihan1.php
+++ phpdasar/pertemuan7/latihan1.php
@@ -4,8 +4,46 @@
 // $_GET["nama"] = "Reza Muhammad";
 // $_GET["nrp"] = "123040224";
 
-var_dump($_GET);
+// var_dump($_GET);
 
+$mahasiswa = [
+    [
+            "nama" => "Reza Muhammad A",
+            "nrp" => "123040224",
+            "email" => "rezamakbar@gmail.com",
+            "jurusan" => "Teknik Informatika",
+            "gambar" => "pp.jpg"
+    ],
+    [
+            "nama" => "Anggya",
+            "nrp" => "123040225",
+            "email" => "anggya@gmail.com",
+            "jurusan" => "Teknik Industri",
+            "gambar" => "pp1.jpg"
 
-
+    ]
+];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GET</title>
+</head>
+<body>
+<h1>Daftar Mahasiswa</h1>
+
+
+<ul>
+<?php foreach ( $mahasiswa as $mhs ) : ?>
+    <li>
+        <a href="latihan2.php?nama=<?= $mhs["nama"]; ?>&nrp=<?= $mhs["nrp"] ;?>&email=<?= $mhs["email"] ?>&jurusan=<?= $mhs["jurusan"]; ?>&gambar<?= $mhs["gambar"]; ?>"><?= $mhs["nama"]; ?></a>
+    </li>
+<?php endforeach; ?>
+</ul>
+
+</body>
+</html>
+
---
+++ phpdasar/pertemuan7/latihan2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detail Mahasiswa</title>
+</head>
+<body>
+
+<ul>
+    <li><img src="img/<?= $mhs["gambar"]; ?>"></li>
+    <li><?= $_GET["nama"]; ?></li>
+    <li><?= $_GET["nrp"]; ?></li>
+    <li><?= $_GET["email"]; ?></li>
+    <li><?= $_GET["jurusan"]; ?></li>
+</ul>
+    
+    <a href="latihan1.php">Kembali ke Daftar Mahasiswa</a>
+
+</body>
+</html>
